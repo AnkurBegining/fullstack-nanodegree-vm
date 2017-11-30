3c54c6e5094c3fab26cffe4ea511738740e1fbf2
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-11-30 16:52:15 +0530

    database_setup.py create with database model

--- vagrant/restaurant/database_setup.py
+++ vagrant/restaurant/database_setup.py
@@ -1,12 +1,28 @@
 import sys
-from sqlalchemy import Integer, String, column, ForeignKey
+from sqlalchemy import Integer, String, Column, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationships
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+    name = Column(String(80), nullable= False)
+    id  = Column(Integer, primary_key= True)
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+    name = Column(String(100), nullable= False)
+    id = Column(Integer,primary_key= True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey(Restaurant.id))
+
+    restaurant = relationship(Restaurant)
+
 
 #### Insert at the end of file #####
 engine = create_engine('sqlite:///restaurantmenu.db')
-Base.metdata.create_all(engine)
+Base.metadata.create_all(engine)

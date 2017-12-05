2393f1f3dc2d42140054ddb1ee49d64a8f3179d0
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-12-05 22:59:01 +0530

    feat: new menu name functionality added

--- vagrant/restaurant/AnkurRestoFlask.py
+++ vagrant/restaurant/AnkurRestoFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -33,10 +33,19 @@ def restaurantMenu(restaurant_id):
 
 
 # Task 1: Create route for newMenuItem function here
-
-@app.route('/restaurants/<int:restaurant_id>/new')
+# form get request and post request added
+@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+
+    if request.method == 'POST':
+        newItem = MenuItem(name= request.form['name'], restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id = restaurant_id)
+
+
 
 # Task 2: Create route for editMenuItem function here
 
